bcae1e5151b2e16d0e9d3259715ac2105b041734
Ilya Chechenev <ilia.chechenev@gmail.com>
2021-11-03 15:13:08 +0300

    added new stuff

---
+++ Cooking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cooking
+//
+//  Created by Ilya Chechenev on 3.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world! new")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cooking/CookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingApp.swift
+//  Cooking
+//
+//  Created by Ilya Chechenev on 3.11.21.
+//
+
+import SwiftUI
+
+@main
+struct CookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
